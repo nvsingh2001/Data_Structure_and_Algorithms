6c8e4956dd004d8cda19a34f9c8f6c39d1ab3392
nvsingh2001 <nvsingh2001@hotmail.com>
2025-11-29 13:39:18 +0530

    Searching Algorithms

---
+++ src/searching/ExponentialSearch.java
@@ -0,0 +1,40 @@
+package searching;
+
+public class ExponentialSearch {
+
+    public static boolean binarySearch(int[] array, int low, int high, int target){
+        if(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(array[mid] == target) return true; 
+            else if(array[mid] > target){
+                return binarySearch(array, low, mid - 1, target);
+            }else{
+                return binarySearch(array, mid + 1, high, target);
+            }
+        }
+        return false;
+    }
+
+    public static boolean exponentialSearch(int[] array, int target){
+        if(array[0] == target){
+            return true;
+        }
+
+        int i = 1;
+        while(i < array.length && array[i] <= target){
+            i *= 2;
+        }
+
+        return binarySearch(array, i/2 , Math.min(i,array.length) , target);
+    }
+
+    public static void main(String[] args) {
+        
+        int[] array = {1,2,4,5,6,99,100,123,235}; 
+
+        System.out.println(exponentialSearch(array, 0));
+        System.out.println(exponentialSearch(array, 100));
+        System.out.println(exponentialSearch(array, 2));
+    }
+}
---
+++ src/searching/TernarySearch.java
@@ -0,0 +1,30 @@
+package searching;
+
+public class TernarySearch {
+
+    public static boolean ternarySearch(int[] array, int low, int high, int element){
+        if(high >= low){
+            int mid1 = low + (high - low) / 3;
+            int mid2 = high - (high - low) / 3;
+
+            if(array[mid1] == element || array[mid2] == element) return true;
+            
+            if(element < array[mid1]){
+                return ternarySearch(array, low, mid1 - 1, element);
+            } 
+            else if(element > array[mid2]){
+                return ternarySearch(array, mid2 + 1, high, element);
+            }else{
+                return ternarySearch(array, mid1 + 1, mid2 - 1, element);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,4,5,6,99,100,123,235}; 
+        System.out.println(ternarySearch(array, 0, array.length - 1, 0));
+        
+        System.out.println(ternarySearch(array, 0, array.length - 1, 100));
+    }
+}
